5bd02a1aef6b45c5423e21c21aab960362bababf
LukashevychAndriy <andriy.lukashewich@gmail.com>
2021-10-19 16:31:54 +0300

    init commit

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const R = +prompt('R = '); // Запрошуємо ввід значення вхідного параметру R
-const xp = +prompt('Введіть х початкове'); // Запрошуємо ввід x початкового
-const xk = +prompt('Введіть х кінцеве'); // Запрошуємо ввід х кінцевого
-const dx = +prompt('Введіть крок'); // Запрошуємо ввід кроку
+const s = +prompt('Введіть значення s'); // Запрошуємо ввід значення вхідного параметру s
+const t = +prompt('Введіть значення t'); // Запрошуємо ввід значення вхідного параметру t
 
-console.log('---------------------------');
-console.log('|\tx\t|\ty\t|');
-console.log('---------------------------');
-
-let y; // Результат обчислень;
-
-for (let x = xp; x <= xk; x += dx) {
-  if (x <= -6) {
-    y = R;
-  } else if (x > -6 && x <= -R) {
-    y = (x + R) / (-R + 6); 
-  } else if (x > -R && x <= 0) {
-    y = R - Math.sqrt(R ** 2 - (x + R) ** 2);
-  } else if (x > 0 && x <= R) {
-    y = Math.sqrt(R ** 2 - x ** 2);
-  } else if (x > R) {
-    y = -x + R;
-  }
-
-  console.log(`|\t${x.toFixed(2)}\t|\t${y.toFixed(3)}\t|`);
+function f (a, b, c) {
+  return a * Math.sin(b) + b * Math.sin(a) + c * c;
 }
+
+let result = (f(1, t + s, s) + f(t, s * t, 1)) / (1 + Math.pow(f(s, 1, t), 2));
+
+console.log(result);
