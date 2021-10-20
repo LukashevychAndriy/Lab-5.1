a4699bdef6db93180174553cbd7b6de1990f857d
LukashevychAndriy <andriy.lukashewich@gmail.com>
2021-10-20 18:36:29 +0300

    changed js file with functions name

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {f} = require('./test');
+const {f} = require('./functions');
 
 test('Function f should return value of an expression a * Math.sin(b) + b * Math.sin(a) + c * c', () => {
   expect(f(0, 0, 0)).toBe(0);
